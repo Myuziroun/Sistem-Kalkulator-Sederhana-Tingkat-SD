9fa5fe2f484dd9262b73bf17418a635d21c21adc
Myuziroun <alifrajafatahillah15@gmail.com>
2024-04-27 09:55:23 +0700

    Menambahkan satuan Jarak

---
+++ satuanJarak.py
@@ -0,0 +1,72 @@
+
+# Konversi Satuan Jarak
+
+# Centimeter
+# 1. Centimeter ke Meter
+def  centimeter_ke_meter(centimeter):
+    return centimeter / 100
+# 2. Centimeter ke Kilometer
+def centimeter_ke_kilomter(centimeter):
+    return centimeter / 100000
+# 3. Centimeter ke Mil 
+def centimeter_ke_mil(centimeter) :
+    return centimeter / 160939
+
+# Meter
+# 1. Meter ke Kilometer
+def meter_ke_kilometer(meter):
+    return meter / 1000
+# 2. Meter ke Mil
+def  meter_ke_mil(meter):
+    return meter / 1609
+# 3. Meter ke Centimeter
+def meter_ke_centimeter(meter):
+    return  meter * 100
+
+# Kilometer 
+# 1. Kilometer ke Centimeter 
+def kilometer_ke_centimeter(kilometer):
+    return kilometer * 100000
+# 2. Kilometre ke Meter
+def kilometer_ke_meter(kilometer):
+    return kilometer * 1000
+# 3. Kilometre ke Mil
+def kilometer_ke_mil(kilometer):
+    return kilometer * 0.6
+ 
+# Mil
+# 1. Mil ke Centimeter
+def mil_ke_centimeter(mil):
+    return mil * 160939
+# 2. Mil ke Meter
+def mil_ke_meter(mil):
+    return mil * 1609
+# 3. Mil ke Kilometer 
+def  mil_ke_kilometer(mil):
+     return mil * 1.6
+
+
+# x1 = int(input("Masukan nilai dalam bentuk centimeter: "))
+# x2 = int(input("Masukan nilai dalam bentuk meter: "))
+# x3 = int(input("Masukan nilai dalam bentuk kilometer: "))
+# x4 = int(input("Masukan nilai dalam bentuk mil: "))
+# print("\n")
+# print("Jarak dalam {} centimeter sebesar {} meter" .format(x1, centimeter_ke_meter(x1)))
+# print("Jarak dalam {} centimeter sebesar {} kilometer" .format(x1, centimeter_ke_kilomter(x1)))
+# print("Jarak dalam {} centimeter sebesar {} mil" .format(x1, centimeter_ke_mil(x1)))
+# print("\n")
+# print("Jarak dalam {} meter sebesar {} centimeter" .format(x2, meter_ke_centimeter(x2)))
+# print("Jarak dalam {} meter sebesar {} kilometer" .format(x2, meter_ke_kilometer(x2)))
+# print("Jarak dalam {} meter sebesar {} mil" .format(x2, meter_ke_mil(x2)))
+# print("\n")
+# print("Jarak dalam {} kilometer sebesar {} centimeter" .format(x3, kilometer_ke_centimeter(x3)))
+# print("Jarak dalam {} kilometer sebesar {} meter" .format(x3, kilometer_ke_meter(x3)))
+# print("Jarak dalam {} kilometer sebesar {} mil" .format(x3, kilometer_ke_mil(x3)))
+# print("\n")
+# print("Jarak dalam {} mil sebesar {} centimeter" .format(x4, mil_ke_centimeter(x4)))
+# print("Jarak dalam {} mil sebesar {} meter" .format(x4, mil_ke_meter(x4)))
+# print("Jarak dalam {} mil sebesar {} kilometer" .format(x4, mil_ke_kilometer(x4)))
+
+
+
+
