8fdaef93938984c90b909d135a97ddbae1048b10
Myuziroun <alifrajafatahillah15@gmail.com>
2024-04-27 10:18:47 +0700

    penambahan fungsi input dan import function dari file yang lain

---
+++ main.py
@@ -0,0 +1,14 @@
+from operasiAritmatika import penjumlahan, pengurangan, pembagian, perkalian;
+from rumusBangunDatarRuang import *;
+from satuanJarak import centimeter_ke_kilomter, centimeter_ke_meter, centimeter_ke_mil, meter_ke_centimeter, meter_ke_kilometer, meter_ke_mil, mil_ke_centimeter, mil_ke_kilometer, mil_ke_meter, kilometer_ke_centimeter, kilometer_ke_meter, kilometer_ke_mil;
+from satuanWaktu import second_to_minute, second_to_hour,  minute_to_hour, minute_to_second, hour_to_minute, hour_to_second;
+
+
+while True :
+    print("Menu Utama")
+    print("1. Perhitungan Aritmatika (+, -, /, *) ")
+    print("2. Perhitungan Bangun Datar ")
+    print("3. Perhitungan Bangun Ruang ")
+    print("4. Perhitungan Perubahan Waktu ")
+    print("5. Perhitungan Perubahan Jarak ")
+    
